1112c44eaf462b3dbb7215ae204803c22326b089
Myron <josephonyekachi77@gmail.com>
2022-10-09 13:50:29 -0800

    PHP Course For Absolute Beginers

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-For-Absolute-Beginers</title>
+</head>
+<body>
+    <!-- Having two Php Tags in same Document -->
+
+    <?php
+        echo"Hello World";
+        echo '<br>';
+        echo " We can also use print to output PHP";
+        echo '<br>'
+    ?>
+
+    <?php
+        // print and echo to output PHP
+        echo"Hello World";
+        echo '<br>';
+        print('Helo world from another php tag');
+        print('<br>');
+        print('<br>');
+        print('<button>Click</button>');
+        print('<br>');
+
+
+        //Comments in PHP
+
+            // Single Line Comment
+
+            # Another Single Line Comment
+
+            /**
+             * Multiple
+             * Line
+             * Commment
+             */
+
+        
+        // to declear variable
+        $name = 'Joeboy';
+        $isMarried = true;
+        $num1 = 4;
+        $num2 = 3;
+        $num3 = 5;
+        $sum = $num1 + $num1 + $num3.' is the sum of num1, num2, num3';
+
+
+        // to print variable
+        print($sum).'<br>';
+        echo $name.'<br>';
+        echo $num1.'<br>';
+        echo $sum.'<br>';
+        echo $isMarried.'<br>';
+        echo '<br>';
+
+
+        // to get type of variable
+        echo gettype($name).'<br>';
+        echo gettype($num1).'<br>';
+        echo gettype($sum).'<br>';
+        echo gettype($isMarried).'<br>';
+
+
+        // to print all the variable and get information
+        var_dump($name, $num1, $isMarried).'<br>';
+
+
+        // variable checking funtion
+        echo '<br>';
+        echo is_string($name).'<br>';
+        echo is_int($num1).'<br>';
+        echo is_bool($isMarried).'<br>';
+
+
+        //checking iif a variable is declearded or not
+        echo '<br>';
+        echo isset($name).'<br>';
+        echo isset($person).'<br>'; // false, so it returns empty = 0
+
+
+        //Constants: This variables Cant Be Changed
+        define('PI', 3.142).'<br>';
+        echo PI.'<br>'; //You can Print the constant variable without $ sign
+
+        //Built-in PHP Constants
+        echo SORT_ASC.'<br>';
+        echo PHP_INT_MAX.'<br>';
+    ?>
+
+
+
+</body>
+</html>
---
+++ numbers.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Numbers in php -->
+    
+    <?php
+        //Declaring Numbers
+        $a = 5;
+        $b = 4;
+        $c = 1.2;
+
+
+        //Arithemetic Operations
+        echo $a + $b.'<br>';
+        echo $a + $b + $c.'<br>';
+        echo $a * $b.'<br>';
+        echo $a - $b.'<br>';
+        echo $a / $b.'<br>';
+        echo $a % $b.'<br>'; //Modulus Operator
+        echo $a + $b * ($a + $c).'<br>';
+
+
+        //Assignment with Math Operators
+        $a += $b; echo $a.'<br>'; // = 9
+        $a -= $b; echo $a.'<br>'; // = 5
+        $a *= $b; echo $a.'<br>'; // = 20
+        $a /= $b; echo $a.'<br>'; // = 5
+
+
+        //Increment Oparator
+        $b++; //Post Increment
+        echo $b.'<br>';
+
+        ++$b; //Pre Increment
+        echo $b.'<br>';
+
+
+        //Decrement Oparetor
+        $b--; //Post Decrement
+        echo $b.'<br>';
+        
+        --$b; //Pre Decrement
+        echo $b.'<br>';
+
+        //Number Checking funtion
+        echo is_float($c).'<br>'; //true
+        echo is_int($a).'<br>'; //true
+        echo is_numeric($a).'<br>'; //true
+
+
+        //conversion
+        $strNum = '4.43';
+        $number = (float)$strNum;
+        $number2 = (int)$strNum;
+        var_dump($number).'<br>';
+        var_dump($number2).'<br>';
+
+
+        //Number function
+        echo '<br>';
+        echo abs(-4).'<br>';
+        echo pow(5, 2).'<br>';
+        echo min(5, 2).'<br>';
+        echo max(5, 2).'<br>';
+        echo round(5.7).'<br>';
+        echo floor(5.7).'<br>';
+
+
+        //Formatting Nmubers
+        $number = 987123654.876543;
+        $formatted = number_format($number, 2, '.', ',');
+        echo $formatted
+
+    ?>
+</body>
+</html>
