068bf7aed71efa87e57cc3c0031f6aa2fff37cf4
Myron <josephonyekachi77@gmail.com>
2022-10-10 12:42:16 -0800

    Php Lesson

---
+++ 10_include_require/function/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'math.php';
+
+echo add(2, 4).'<br>';
+
+echo sub(2, 4).'<br>';
+
+echo mul(2, 4).'<br>';
+
+
+
+
+
+?>
---
+++ 10_include_require/function/math.php
@@ -0,0 +1,15 @@
+<?php
+
+function add($a, $b){
+    return $a + $b;
+}
+
+function sub($a, $b){
+    return $a - $b;
+}
+
+function mul($a, $b){
+    return $a * $b;
+}
+
+
