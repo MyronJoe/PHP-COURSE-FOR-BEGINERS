78bee361ed66a09771d4015b6c1313d6a3a6043f
Myron <josephonyekachi77@gmail.com>
2022-10-09 16:34:06 -0800

    Starting the loop lesson

---
+++ loops.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conditional-PHP</title>
+</head>
+<body>
+    <!-- Conditional in php -->
+    
+    <?php
+        $welcome = 'Welcome To Our Loop Class';
+        echo $welcome.'<br>'.'<br>';
+        echo '<br>';
+
+        //While
+
+        //Loop with $counter
+
+
+        //do-while
+
+
+        //for
+
+
+        //foreach
+
+
+        //iterate Over Associative Array
+        
+
+    ?>
+</body>
+</html>
