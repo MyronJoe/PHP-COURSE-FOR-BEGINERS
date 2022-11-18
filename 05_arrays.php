89fe08227309fe62727697e5eaa479e0dbfbbce2
Myron <josephonyekachi77@gmail.com>
2022-11-17 21:16:41 -0800

    updated

--- 05_arrays.php
+++ 05_arrays.php
@@ -1,223 +1,226 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Array-PHP</title>
 </head>
+
 <body>
     <!-- Array in php -->
-    
+
     <?php
-        $welcome = 'Welcome To Our Array Class';
-        echo $welcome.'<br>';
+    $welcome = 'Welcome To Our Array Class';
+    echo $welcome . '<br>';
 
-        //Creating Array
-        $fruits = ['Orange', 'Banana', 'Apple', 'Mango',];
-        $cars = ['Volvo', 'Benz', 'Toyota', 'Honda',];
+    //Creating Array
+    $fruits = ['Orange', 'Banana', 'Apple', 'Mango',];
+    $cars = ['Volvo', 'Benz', 'Toyota', 'Honda',];
 
 
-        //print the whole array
-        var_dump($fruits).'<br>';
+    //print the whole array
+    var_dump($fruits) . '<br>';
 
-        echo '<pre>';
-            var_dump($fruits).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($fruits) . '<br>';
+    echo '</pre>';
 
-        //get element by index
-        echo($fruits[0]).'<br>';
-        echo($fruits[1]).'<br>';
-        echo($fruits[3]).'<br>';
+    //get element by index
+    echo ($fruits[0]) . '<br>';
+    echo ($fruits[1]) . '<br>';
+    echo ($fruits[3]) . '<br>';
 
-        //set element by index
-        $fruits[1] = 'Cashew';
-        $fruits[4] = 'Almond';
+    //set element by index
+    $fruits[1] = 'Cashew';
+    $fruits[4] = 'Almond';
 
-        echo '<pre>';
-            var_dump($fruits).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($fruits) . '<br>';
+    echo '</pre>';
 
-        //check if array has elemnt at index
-        echo isset($fruits[2]).'<br>'; //true
-        echo isset($fruits[5]).'<br>'; //false
-        echo isset($fruits[0]).'<br>'; //true
+    //check if array has elemnt at index
+    echo isset($fruits[2]) . '<br>'; //true
+    echo isset($fruits[5]) . '<br>'; //false
+    echo isset($fruits[0]) . '<br>'; //true
 
 
-        //append element
-         $fruits[] = 'Peach';
+    //append element
+    $fruits[] = 'Peach';
 
-         echo '<pre>';
-            var_dump($fruits).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($fruits) . '<br>';
+    echo '</pre>';
 
 
-        //print the lenght of arry 
-        echo count($fruits).'<br>';
+    //print the lenght of arry 
+    echo count($fruits) . '<br>';
 
 
-        //add element at end of an array
-        array_push($fruits, 'Pawpaw');
+    //add element at end of an array
+    array_push($fruits, 'Pawpaw');
 
-        echo '<pre>';
-            var_dump($fruits).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($fruits) . '<br>';
+    echo '</pre>';
 
 
-        //remove element from the end of an array
-        echo array_pop($fruits);
+    //remove element from the end of an array
+    echo array_pop($fruits);
 
-        echo '<pre>';
-            var_dump($fruits).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($fruits) . '<br>';
+    echo '</pre>';
 
 
-        //add element to the beginning of an array
-        array_unshift($fruits, 'foo');
+    //add element to the beginning of an array
+    array_unshift($fruits, 'foo');
 
-        echo '<pre>';
-            var_dump($fruits).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($fruits) . '<br>';
+    echo '</pre>';
 
 
-        //remove element from the beginning of an array
-        echo array_shift($fruits);
+    //remove element from the beginning of an array
+    echo array_shift($fruits);
 
-        echo '<pre>';
-            var_dump($fruits).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($fruits) . '<br>';
+    echo '</pre>';
 
 
-        //split the string into an array
-        $str = "Banana Apple Peach";
-        
+    //split the string into an array
+    $str = "Banana Apple Peach";
 
-        echo '<pre>';
-            var_dump(explode(" ", $str)).'<br>';
-        echo '</pre>';
 
+    echo '<pre>';
+    var_dump(explode(" ", $str)) . '<br>';
+    echo '</pre>';
 
-        //combine array into string
-        echo implode(", ", $fruits).'<br>';
 
+    //combine array into string
+    echo implode(", ", $fruits) . '<br>';
 
-        //check if element exist in an array
-        echo in_array('Apple', $fruits).'<br>';
-        var_dump(in_array('Apple', $fruits)).'<br>';
-        var_dump(in_array('pinapple', $fruits)).'<br>';
 
+    //check if element exist in an array
+    echo in_array('Apple', $fruits) . '<br>';
+    var_dump(in_array('Apple', $fruits)) . '<br>';
+    var_dump(in_array('pinapple', $fruits)) . '<br>';
 
-        // search element index in the array
-        echo '<pre>';
-        var_dump(array_search('pinapple', $fruits)).'<br>';
-        echo '</pre>';
-        echo '<pre>';
-        var_dump(array_search('Apple', $fruits)).'<br>';
-        echo '</pre>';
 
-        //merge two arrays
+    // search element index in the array
+    echo '<pre>';
+    var_dump(array_search('pinapple', $fruits)) . '<br>';
+    echo '</pre>';
+    echo '<pre>';
+    var_dump(array_search('Apple', $fruits)) . '<br>';
+    echo '</pre>';
 
-        echo '<pre>';
-        var_dump(array_merge($cars, $fruits)).'<br>';
-        var_dump([...$fruits, ...$cars]).'<br>';
-        echo '</pre>';
+    //merge two arrays
 
+    echo '<pre>';
+    var_dump(array_merge($cars, $fruits)) . '<br>';
+    var_dump([...$fruits, ...$cars]) . '<br>';
+    echo '</pre>';
 
-        //sorting array
-        $concate = [...$fruits, ...$cars];
 
-        sort($concate);
+    //sorting array
+    $concate = [...$fruits, ...$cars];
 
-        echo '<pre>';
-        var_dump($concate).'<br>';
-        echo '</pre>';
+    sort($concate);
 
-        //Reverse sorting array
-        rsort($concate);
+    echo '<pre>';
+    var_dump($concate) . '<br>';
+    echo '</pre>';
 
-        echo '<pre>';
-        var_dump($concate).'<br>';
-        echo '</pre>';
-        
+    //Reverse sorting array
+    rsort($concate);
 
-        //Associative array
-        $person = [
-            'name' => 'Joe',
-            'age' => 20,
-            'location' => 'Africa',
-            'Hobbies' => ['Coding', 'Reading'],
-            // 'address' => 34
-        ];
+    echo '<pre>';
+    var_dump($concate) . '<br>';
+    echo '</pre>';
 
-        echo '<pre>';
-        var_dump($person).'<br>';
-        echo '</pre>';
 
-        //Accessing an associative array
+    //Associative array
+    $person = [
+        'name' => 'Joe',
+        'age' => 20,
+        'location' => 'Africa',
+        'Hobbies' => ['Coding', 'Reading'],
+        // 'address' => 34
+    ];
 
-        echo $person['name'].'<br>';
-        echo $person['age'].'<br>';
+    echo '<pre>';
+    var_dump($person) . '<br>';
+    echo '</pre>';
 
-        //set element in an associative array
-        $person['Sname'] = 'Myron';
-        echo '<pre>';
-        var_dump($person).'<br>';
-        echo '</pre>';
+    //Accessing an associative array
 
+    echo $person['name'] . '<br>';
+    echo $person['age'] . '<br>';
 
-        //Null Coalescing assignment operator
+    //set element in an associative array
+    $person['Sname'] = 'Myron';
+    echo '<pre>';
+    var_dump($person) . '<br>';
+    echo '</pre>';
 
-        // if (!isset($person['address'])) {
-        //     $person['address'] = 'Unknown';
-        // }
 
-        $person['address'] ??= 'Unknown';
+    //Null Coalescing assignment operator
 
-        echo '<pre>';
-        var_dump($person).'<br>';
-        echo '</pre>';
+    // if (!isset($person['address'])) {
+    //     $person['address'] = 'Unknown';
+    // }
 
+    $person['address'] ??= 'Unknown';
 
-        //print the key of the array
-        echo '<pre>';
-        var_dump(array_keys($person)).'<br>';
-        echo '</pre>';
+    echo '<pre>';
+    var_dump($person) . '<br>';
+    echo '</pre>';
 
 
-        //print the values of the array
-        echo '<pre>';
-        var_dump(array_values($person)).'<br>';
-        echo '</pre>';
+    //print the key of the array
+    echo '<pre>';
+    var_dump(array_keys($person)) . '<br>';
+    echo '</pre>';
 
 
-        //Sort the array with keys
-        ksort($person);
-        echo '<pre>';
-        var_dump(array_values($person)).'<br>';
-        echo '</pre>';
+    //print the values of the array
+    echo '<pre>';
+    var_dump(array_values($person)) . '<br>';
+    echo '</pre>';
 
 
-        //Sort the array with values
-        asort($person);
-        echo '<pre>';
-        var_dump(array_values($person)).'<br>';
-        echo '</pre>';
+    //Sort the array with keys
+    ksort($person);
+    echo '<pre>';
+    var_dump(array_values($person)) . '<br>';
+    echo '</pre>';
 
 
-        //Two dimensional array
-        $todos = [
-            ['title'=>'Todo title 1', 'Activity1'=>'Coding'],
-            ['title'=>'Todo title 2', 'Activity2'=>'Reading']
-        ];
+    //Sort the array with values
+    asort($person);
+    echo '<pre>';
+    var_dump(array_values($person)) . '<br>';
+    echo '</pre>';
 
-        echo '<pre>';
-        var_dump(array_values($todos)).'<br>';
-        echo '</pre>';
 
-        echo '<pre>';
-        var_dump(array_values($todos[0])).'<br>';
-        echo '</pre>';
+    //Two dimensional array
+    $todos = [
+        ['title' => 'Todo title 1', 'Activity1' => 'Coding'],
+        ['title' => 'Todo title 2', 'Activity2' => 'Reading']
+    ];
+
+    echo '<pre>';
+    var_dump(array_values($todos)) . '<br>';
+    echo '</pre>';
+
+    echo '<pre>';
+    var_dump(array_values($todos[0])) . '<br>';
+    echo '</pre>';
 
     ?>
 </body>
+
 </html>
